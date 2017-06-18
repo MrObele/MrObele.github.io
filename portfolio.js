b69b5a33835d3fa6463a1ecd8267bbdb0f2869aa
MrObele <obeledavid@yahoo.com>
2017-06-18 11:39:30 +0100

    updated weather app image in portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
 
   //all projects
-     var allImageArray = ["images/walt-disney.jpg","images/randomquote1.jpg","images/Weather.jpg","images/wikipediaviewer.jpg","images/twitchStreamers1.jpg","images/javascriptcalculator1.jpg","images/pomodoroclock1.jpg","images/tictactoe1.jpg","images/simon.jpg"];
+     var allImageArray = ["images/walt-disney.jpg","images/randomquote1.jpg","images/weather.jpg","images/wikipediaviewer.jpg","images/twitchStreamers1.jpg","images/javascriptcalculator1.jpg","images/pomodoroclock1.jpg","images/tictactoe1.jpg","images/simon.jpg"];
      var allProjects=["Tribute Page","Random Quote Machine","Local Weather App","Wikipedia Viewer" ,"Twitchtv Streamers","JavaScript Calculator","Pomodoro Clock","Tic Tac Toe Game","Simon Game"];
   var allProjectLinks = ["https://codepen.io/davesoft/full/BKMNLB/","https://codepen.io/davesoft/full/LZEgOM/","https://codepen.io/davesoft/full/bedLyw/","https://codepen.io/davesoft/full/PzPbaM/" ,"https://codepen.io/davesoft/full/rLxezM/","https://codepen.io/davesoft/full/LxrEGj/","https://codepen.io/davesoft/full/QdVMEV/","https://codepen.io/davesoft/full/Rpbbxq/","https://codepen.io/davesoft/full/mmYoXJ/"];
   //all projects div
